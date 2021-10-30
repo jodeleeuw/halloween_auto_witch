30e0f83bfc2e4ccb9b162d5b9d3ef6bf0af294f9
Josh de Leeuw <josh.deleeuw@gmail.com>
2021-10-30 17:13:44 -0400

    retract and extend code

--- motordeck/motordeck.ino
+++ motordeck/motordeck.ino
@@ -4,18 +4,55 @@ Servo pwm;
 
 void setup() {
   // put your setup code here, to run once:
-
   pwm.attach(11);
 }
 
+
+// 1050 is clockwise looking at the motor
+// 1950 is counterclockwise looking at the motor
+
+// 1500 is neutral.
+
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(1000);
-  pwm.writeMicroseconds(1500);
-  delay(1000);
-  pwm.writeMicroseconds(1950);
-  delay(1000);
-  pwm.writeMicroseconds(1050);
-  delay(1000);
-  pwm.writeMicroseconds(1500);
+  
+  sendDown(5000);
+
+  delay(5000);
+
+  sendUp(5000);
+
+  delay(5000);
+}
+
+void sendDown(int duration){
+  int total_time = 0;
+  for(int pulse = 1500; pulse < 1950; pulse +=10){
+    pwm.writeMicroseconds(pulse);
+    delay(100);
+    total_time += 100;
+  }
+
+  delay(duration - total_time);
+
+  for(int pulse = 1950; pulse >= 1500; pulse -=10){
+    pwm.writeMicroseconds(pulse);
+    delay(100);
+  }
+}
+
+void sendUp(int duration){
+  int total_time = 0;
+  for(int pulse = 1500; pulse > 1050; pulse -=10){
+    pwm.writeMicroseconds(pulse);
+    delay(100);
+    total_time += 100;
+  }
+
+  delay(duration - total_time);
+
+  for(int pulse = 1050; pulse <= 1500; pulse +=10){
+    pwm.writeMicroseconds(pulse);
+    delay(100);
+  }
 }
